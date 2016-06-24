7de82627a14d708aa3566f8ae66f696fc69e7724
Abhishek Desai <abhishek.desai70@gmail.com>
2016-06-23 21:57:19 -0700

    Minified the bundl.js file with webpack command

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = express();
 
 // Create Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 //Create Middleware for OpenWeather API (Https/Http)
 app.use(function (req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if(req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
